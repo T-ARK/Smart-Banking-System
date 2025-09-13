cc6d9074d6e2cdffa8ac9fe7b35c0feda73bc4b3
ARK <99589954+T-ARK@users.noreply.github.com>
2025-09-13 11:29:56 +0530

    Update Customer.java

--- model/Customer.java
+++ model/Customer.java
@@ -1,2 +1,20 @@
 package model;
-public class Customer { private int customerId; private String name; private String username; private String password; }
+
+public class Customer {
+    private int userId;
+    private String username;
+
+    public Customer(int userId, String username) {
+        this.userId = userId;
+        this.username = username;
+    }
+
+    // Getters
+    public int getUserId() {
+        return userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+}
