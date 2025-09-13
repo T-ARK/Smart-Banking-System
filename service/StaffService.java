4426402c671a1249c8b5f11cd4a442dcdf26f6c9
ARK <99589954+T-ARK@users.noreply.github.com>
2025-09-13 17:56:19 +0530

    Update StaffService.java

--- service/StaffService.java
+++ service/StaffService.java
@@ -1,2 +1,28 @@
 package service;
-public class StaffService { }
+
+import dao.CustomerDAO;
+import dao.StaffDAO;
+import dao.TransactionDAO;
+import model.Customer;
+import model.Staff;
+import model.Transaction;
+
+import java.util.List;
+
+public class StaffService {
+    private StaffDAO staffDAO = new StaffDAO();
+    private CustomerDAO customerDAO = new CustomerDAO();
+    private TransactionDAO transactionDAO = new TransactionDAO();
+
+    public Staff login(String username, String password) {
+        return staffDAO.login(username, password);
+    }
+
+    public List<Customer> viewCustomers() {
+        return customerDAO.getAllCustomers();
+    }
+
+    public List<Transaction> viewAllTransactions() {
+        return transactionDAO.getAllTransactions();
+    }
+}
