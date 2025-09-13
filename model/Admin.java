4b4b1d8bf056e1332918f42e4b18c0e598279ad3
ARK <99589954+T-ARK@users.noreply.github.com>
2025-09-13 11:31:19 +0530

    Update Admin.java

--- model/Admin.java
+++ model/Admin.java
@@ -1,2 +1,20 @@
 package model;
-public class Admin { private int adminId; private String username; private String password; }
+
+public class Admin {
+    private int userId;
+    private String username;
+
+    public Admin(int userId, String username) {
+        this.userId = userId;
+        this.username = username;
+    }
+
+    // Getters
+    public int getUserId() {
+        return userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+}
